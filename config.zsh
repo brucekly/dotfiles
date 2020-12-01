1c618d53f3b7914bf5fabe344961dfcba32f1abb
Bruce McPhail <brucemcphail@icloud.com>
2020-12-01 18:03:00 +0000

    Fix fzf command

--- config.zsh
+++ config.zsh
@@ -9,7 +9,7 @@ plugins=(
 
 source $ZSH/oh-my-zsh.sh
 
-export FZF_DEFAULT_COMMAND="fd . $HOME"
+export FZF_DEFAULT_COMMAND="fd -t f"
 export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
 export FZF_ALT_C_COMMAND="fd -t d . $HOME"
 
